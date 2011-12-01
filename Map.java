d750923ec177c2ba5a80b0ac79348c9102e3372e
Michael Mattson <mijmattson@gmail.com>
2011-11-30 21:38:59 -0600

    Updated Map to implement the Ship and Player classes

--- Map.java
+++ Map.java
@@ -6,7 +6,7 @@
  * 	11/30/2011
  * 
  * Description:
- * 	Models a 30 X 30 map for a game of battle ship
+ * 	Models a 30 X 30 map for a game of battle ship 
  */
 
 import java.awt.Graphics;
@@ -26,8 +26,7 @@ public class Map {
 	 * Constructs the map specified by the text file
 	 */
 	public Map(String txtFile){
-		// initialize _map
-		// set _map to contain all water using file (reefs and mines may be added here also)
+		// initialize _map using a .txt file
 		// initialize both players
 		// add all ships and bases
 		_map = new SquareTile[30][30];
