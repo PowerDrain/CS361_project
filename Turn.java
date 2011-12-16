296baac4a32ddddd4bd735367fbdd9404adf4a28
David Gamez <powerdrain@gmail.com>
2011-12-15 20:41:28 -0600

    Completed immerseMine and withdrawMine methods.

--- Turn.java
+++ Turn.java
@@ -21,7 +21,7 @@ public class Turn {
 		returnValue[1] = null;
 		boolean shipCanRotate = false;
 		Point[] possrp = currentPlayer.getCurrentShip().getRotationalMobility();
-		
+
 		for (int i = 0; i < possrp.length; i++){
 			if (pointToRotate.equals(possrp[i])){
 				shipCanRotate = true;
@@ -53,7 +53,7 @@ public class Turn {
 			return returnValue;
 		}	
 	}//End of rotateShip
-	
+
 	public String[] moveShip(Point pointToMove){
 		String[] returnValue = new String[2];
 		returnValue[0] = null;
@@ -61,7 +61,7 @@ public class Turn {
 		boolean shipCanMove = false;
 		boolean pointInMobility = false;
 		Point[] possibleMovePoints = currentPlayer.getCurrentShip().getMoveMobility();
-		
+
 		//Test to see if the point is a legal move
 		for (int i = 0; i <possibleMovePoints.length; i++) {
 			if (pointToMove.equals(possibleMovePoints[i])){
@@ -74,7 +74,7 @@ public class Turn {
 			returnValue[1] = "This ship is not able to move to point " + pointToMove + ".";
 			return returnValue;
 		}
-		
+
 		//Testing to see if anything might be in the way
 		for (int i = 0; i < possibleMovePoints.length; i++) {
 			if (currentMap.hasBase(possibleMovePoints[i]) || currentMap.hasMine(possibleMovePoints[i]) || currentMap.hasReef(possibleMovePoints[i]) || currentMap.hasShip(possibleMovePoints[i])){
@@ -99,7 +99,7 @@ public class Turn {
 				return actuallyMoveShip(pointToMove);
 			}
 		}
-		
+
 		//Test for left mobility
 		Point[] leftOfShip = currentPlayer.getCurrentShip().getLeftMobility();
 		boolean moveLeft = false;
@@ -178,7 +178,7 @@ public class Turn {
 		returnValue[1] = "Shouldn't ever see this in console, if you do something is wrong with moveShip in Turn class.";
 		return returnValue;
 	}//End of moveShip
-		
+
 	private String[] actuallyMoveShip(Point pointToMove){
 		String[] returnValue = new String[2];
 		currentPlayer.getCurrentShip().moveShip(pointToMove);
@@ -188,44 +188,128 @@ public class Turn {
 		return returnValue;
 	}
 
-	//TODO
-	/* Fix this method, must return correct return values.
-	 * Cannot use toString method, also check to see that 
-	 * the loc point is adjacent to Dredger ship, this might
-	 * require a method in Dredger class.
-	 */
-	public boolean immerseMine(Point loc){
-		//TODO this might require a isAdjacent method in ship. . .
-		if(currentPlayer.mineCount() <= 0 || currentPlayer.getCurrentShip().toString() != "Dredger") return false;
-		if(currentMap.hasMine(loc) || currentMap.hasBase(loc) || currentMap.hasReef(loc) || currentMap.hasShip(loc))  return false;
-		currentPlayer.decrementMineCount();
-		currentMap.placeMine(loc);
-		return true;
-	}//End of immerseMine Method
+	public String[] immerseMine(Point dropMineLocation){
+		String[] returnValue = new String[2];
+		returnValue[0] = null;
+		returnValue[1] = null;
+		boolean canDropMine = false;
+		if (!(currentPlayer.getCurrentShip() instanceof Dredger)){
+			returnValue[0] = null;
+			returnValue[1] = "Current ship is not a dredger, only dredgers can drop mines.";
+			return returnValue;
+		}
+		Point[] legalDropSpots = currentPlayer.getCurrentShip().getAdjacentPoints();
+		for (int i = 0; i <legalDropSpots.length; i++){
+			if (dropMineLocation.equals(legalDropSpots[i])){
+				canDropMine = true;
+				break;
+			}
+		}
+		if (!canDropMine){
+			returnValue[0] = null;
+			returnValue[1] = "Cannot drop a mine in location that is not adjacent to a dredger.";
+			return returnValue;
+		}
+		if (currentMap.hasBase(dropMineLocation) || currentMap.hasReef(dropMineLocation) || currentMap.hasShip(dropMineLocation) || currentMap.hasMine(dropMineLocation)){
+			returnValue[0] = null;
+			returnValue[1] = "Cannot drop mine at point " + dropMineLocation + " there is something in the way.";
+			return returnValue;
+		}
+		Ship[] allCurrentPlayerShips = currentPlayer.getPlayerShips();
+		for (int i = 0; i < allCurrentPlayerShips.length; i++){
+			Point[] pointsAroundShip = allCurrentPlayerShips[i].getAdjacentPoints();
+			for (int j = 0; j < pointsAroundShip.length; j++){
+				if (dropMineLocation.equals(pointsAroundShip[i])){
+					returnValue[0] = null;
+					returnValue[1] = "Cannot drop a mine right next to a ship, this would cause an instantaneous explosion!";
+					return returnValue;
+				}
+			}
+		}
 
-	//TODO
-	/* Fix this method, must return correct return values.
-	 * Cannot use toString method, also check to see that 
-	 * the loc point is adjacent to Dredger ship, this might
-	 * require a method in Dredger class.
-	 */
-	public boolean withdrawMine(Point loc){
-		if(currentPlayer.getCurrentShip().toString() != "Dredger") return false;
-		//Make sure ship is touching mine;
-		currentMap.removeMine(loc);
-		currentPlayer.decrementMineCount();
-		return true;
-	}//End of withdrawMine Method
+		Ship[] allOpponentShips = opponent.getPlayerShips();
+		for (int i = 0; i < allOpponentShips.length; i++){
+			Point[] pointsAroundShip = allOpponentShips[i].getAdjacentPoints();
+			for (int j = 0; j < pointsAroundShip.length; j++){
+				if (dropMineLocation.equals(pointsAroundShip[i])){
+					returnValue[0] = null;
+					returnValue[1] = "Cannot drop a mine right next to a ship, this would cause an instantaneous explosion!";
+					return returnValue;
+				}
+			}
+		}
+		Point[] pointsAroundBase = currentPlayer.base().getAdjacentPoints();
+		for (int i = 0; i < pointsAroundBase.length; i++){
+			if (dropMineLocation.equals(pointsAroundBase[i])){
+				returnValue[0] = null;
+				returnValue[1] = "Cannot drop a mine right next to a base, this would cause an instantaneous explosion!";
+				return returnValue;
+			}
+		}
+		pointsAroundBase = opponent.base().getAdjacentPoints();
+		for (int i = 0; i < pointsAroundBase.length; i++){
+			if (dropMineLocation.equals(pointsAroundBase[i])){
+				returnValue[0] = null;
+				returnValue[1] = "Cannot drop a mine right next to a base, this would cause an instantaneous explosion!";
+				return returnValue;
+			}
+		}
+
+		if (currentPlayer.decrementMineCount()){
+			currentMap.placeMine(dropMineLocation);
+			returnValue[0] = "0";
+			returnValue[1] = "Mine successfully drop at location " + dropMineLocation + ".";
+		} else {
+			returnValue[0] = null;
+			returnValue[1] = "You can't drop mines joker, you're out of mines!";
+		}
+		return returnValue;
+	}
+
+	public String[] withdrawMine(Point pickupMineLocation){
+		String[] returnValue = new String[2];
+		returnValue[0] = null;
+		returnValue[1] = null;
+		boolean canPickupMine = false;
+		if (currentMap.hasMine(pickupMineLocation)){
+			if (!(currentPlayer.getCurrentShip() instanceof Dredger)){
+				returnValue[0] = null;
+				returnValue[1] = "Current ship is not a dredger, only dredgers can pickup mines.";
+				return returnValue;
+			}
+			Point[] legalPickupSpots = currentPlayer.getCurrentShip().getAdjacentPoints();
+			for (int i = 0; i <legalPickupSpots.length; i++){
+				if (pickupMineLocation.equals(legalPickupSpots[i])){
+					canPickupMine = true;
+					break;
+				}
+			}
+			if (!canPickupMine){
+				returnValue[0] = null;
+				returnValue[1] = "Cannot pick up a mine in location that is not adjacent to a dredger.";
+				return returnValue;
+			} else {
+				currentPlayer.incrementMineCount();
+				currentMap.removeMine(pickupMineLocation);
+				returnValue[0] = "0";
+				returnValue[1] = "Mine successfully removed from point " + pickupMineLocation + ".";
+			}
+		} else {
+			returnValue[0] = null;
+			returnValue[1] = "Cannot pickup mine, no mine at location " + pickupMineLocation + ".";
+		}
+		return returnValue;
+	}
 
 	public String[] launchTorpedo() throws IllegalStateException {
 		String[] returnValue = new String[2];
 		returnValue[0] = null;
 		returnValue[1] = null;
-		
+
 		if (currentPlayer.getCurrentShip() instanceof Destroyer || currentPlayer.getCurrentShip() instanceof Torpedo){
 			int xCoor = currentPlayer.getCurrentShip().getPosition().x;
 			int yCoor = currentPlayer.getCurrentShip().getPosition().y;
-			
+
 			switch (currentPlayer.getCurrentShip().getDirection()){
 			case 'n':
 				for (int i = 1; (i <= 10)&&(yCoor >= 0); i++){
@@ -356,7 +440,7 @@ public class Turn {
 					returnValue[0] = null;
 					returnValue[1] = "Current ship is at full health, cannot repair it.";
 				}
-				
+
 			} else {
 				returnValue[0] = null;
 				returnValue[1] = "Current ship is not docked at a base, cannot repair it.";
@@ -368,6 +452,9 @@ public class Turn {
 		return returnValue;
 	}
 
+	public int getTurnNuber(){
+		return turnNumber;
+	}
 	public String[] repairBase(){
 		String[] returnValue = new String[2];
 		returnValue[0] = null;
@@ -386,7 +473,7 @@ public class Turn {
 	public int getMineCount(){
 		return currentPlayer.mineCount();
 	}//End of getMineCount()
-	
+
 	public Map getMap(){
 		return currentMap;
 	}//End of getMap
